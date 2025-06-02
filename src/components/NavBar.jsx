693cc64acb5d3625a0ac01bbe427cd3ff8a68bb7
sandeep <sandeeps16x@gmail.com>
2025-06-02 21:40:01 +0530

    icon

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -22,13 +22,13 @@ const NavBar = () => {
     <div className="navbar bg-base-300 shadow-sm">
       <div className="flex-1">
         <Link to={"/"} className="btn btn-ghost text-xl">
-          DEVTINDER
+          🧑🏻‍💻DEVTINDER
         </Link>
       </div>
 
       {user && (
         <>
-          <p>Welcome {user?.firstName}</p>
+          <p>Welcome, {user?.firstName}</p>
           <div className="flex gap-2 ">
             <div className="dropdown dropdown-end mx-5">
               <div
