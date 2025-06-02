e2dae7562fbcab02a862466d33ca16eec9471dfd
sandeep <sandeeps16x@gmail.com>
2025-06-02 21:50:40 +0530

    navbar

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -18,11 +18,12 @@ const NavBar = () => {
       console.error(err);
     }
   };
+
   return (
     <div className="navbar bg-base-300 shadow-sm">
       <div className="flex-1">
         <Link to={"/"} className="btn btn-ghost text-xl">
-          🧑🏻‍💻DEVTINDER
+          🧑🏻‍💻 DEVTINDER
         </Link>
       </div>
 
